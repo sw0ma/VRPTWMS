a644c4b3ff245b38e1582f1cf5e3a076c6162b2b
Michael Walter <mwlter@aol.com>
2015-04-12 20:53:18 +0200

    functions reorderd consumer to customer renamend config added

--- VRPTWMS/src/data/mVRPTWMS/VRPTWMSInstance.java
+++ VRPTWMS/src/data/mVRPTWMS/VRPTWMSInstance.java
@@ -9,70 +9,99 @@ import util.ownDataStructure.DuoHashMap;
 import data.AArc;
 import data.AInstance;
 import data.AVertice;
-import data.Config;
 
 public class VRPTWMSInstance extends AInstance {
 
-	private Map<String, Consumer> consumer;
+	private VRPTWMSConfig config;
+	private Map<String, Customer> customer;
 	private Map<String, Depot> depots;
 	private DuoHashMap<String, String, AArc> arcs;
-	private Config config;
 
+	
 	public VRPTWMSInstance() {
-		consumer = new HashMap<String, Consumer>();
+		customer = new HashMap<String, Customer>();
 		depots = new HashMap<String, Depot>();
 		arcs = new DuoHashMap<String, String, AArc>();
 	}
+	
+	
+	@Override
+	public VRPTWMSConfig getConfig() {
+		if(this.config == null) {
+			this.config = (VRPTWMSConfig) VRPTWMSConfig.createNewConfig();
+		}
+		return this.config;
+	}
+	
+	@Override
+	public void setConfig(VRPTWMSConfig config) {
+		if(config != null) {
+			this.config = config;
+		}
+	}
+	
 
 	@Override
+	public AVertice getVertice(String name) {
+		AVertice result = customer.get(name);
+		if(result == null) {
+			return depots.get(name);
+		}
+		return result;
+	}
+	
+	@Override
 	public void addVertice(AVertice pVerticeToAdd) {
-		if (pVerticeToAdd instanceof Consumer) {
-			this.consumer.put(pVerticeToAdd.getName(), (Consumer) pVerticeToAdd);
+		if (pVerticeToAdd instanceof Customer) {
+			this.customer.put(pVerticeToAdd.getName(), (Customer) pVerticeToAdd);
 		} else {
 			this.depots.put(pVerticeToAdd.getName(), (Depot) pVerticeToAdd);
 		}
 	}
-
+	
 	@Override
-	public List<Consumer> getConsumer() {
-		return new ArrayList<Consumer>(consumer.values());
+	public List<AVertice> getVertices() {
+		List<AVertice> allVertices = new ArrayList<AVertice>();
+		allVertices.addAll(depots.values());
+		allVertices.addAll(customer.values());
+		return allVertices;
 	}
-
-	@Override
-	public List<Depot> getDepots() {
-		return new ArrayList<Depot>(depots.values());
-	}
-
+	
 	@Override
 	public void setVertices(List<AVertice> vertices) {
-		this.consumer = new HashMap<String, Consumer>();
+		this.customer = new HashMap<String, Customer>();
 		for (AVertice vertice : vertices) {
-			if (vertice instanceof Consumer) {
-				this.consumer.put(vertice.getName(), (Consumer) vertice);
+			if (vertice instanceof Customer) {
+				this.customer.put(vertice.getName(), (Customer) vertice);
 			} else {
 				this.depots.put(vertice.getName(), (Depot) vertice);
 			}
 		}
 	}
 
+	
 	@Override
-	public boolean setArcs(List<AArc> arcs) {
-		this.arcs = new DuoHashMap<String, String, AArc>();
-		for (AArc arc : arcs) {
-			if ((consumer.get(arc.getFrom().getName()) == null && depots.get(arc.getFrom().getName()) == null)
-					|| (consumer.get(arc.getTo().getName()) == null && depots.get(arc.getTo().getName()) == null)) {
-				System.out.println(consumer.get(arc.getFrom().getName()));
-				System.out.println(depots.get(arc.getFrom().getName()));
-				System.out.println(consumer.get(arc.getTo().getName()));
-				System.out.println(depots.get(arc.getTo().getName()));
-				this.arcs = new DuoHashMap<String, String, AArc>();
-				return false;
-			}
-			this.arcs.put(arc.getFrom().getName(), arc.getTo().getName(), arc);
-		}
-		return true;
+	public List<Customer> getCustomers() {
+		return new ArrayList<Customer>(customer.values());
+	}
+	
+	@Override
+	public Customer getCustomer(String name) {
+		return customer.get(name);
+	}
+	
+
+	@Override
+	public List<Depot> getDepots() {
+		return new ArrayList<Depot>(depots.values());
 	}
 
+	
+	@Override
+	public AArc getArc(AVertice v1, AVertice v2) {
+		return arcs.get(v1.getName(), v2.getName());
+	}
+	
 	@Override
 	public boolean addArc(AArc arc) {
 		if (getVertice(arc.getFrom().getName()) == null || getVertice(arc.getTo().getName()) == null) {
@@ -81,54 +110,30 @@ public class VRPTWMSInstance extends AInstance {
 		arcs.put(arc.getFrom().getName(), arc.getTo().getName(), arc);
 		return true;
 	}
-
-	@Override
-	public AArc getArc(AVertice v1, AVertice v2) {
-		return arcs.get(v1.getName(), v2.getName());
-	}
-
+	
 	@Override
 	public List<AArc> getArcs() {
 		List<AArc> arcList = new ArrayList<AArc>();
 		arcList.addAll(arcs.values());
 		return arcList;
 	}
-
+	
 	@Override
-	public Consumer getConsumer(String name) {
-		return consumer.get(name);
-	}
-
-	@Override
-	public AVertice getVertice(String name) {
-		AVertice result = consumer.get(name);
-		if(result == null) {
-			return depots.get(name);
+	public boolean setArcs(List<AArc> arcs) {
+		this.arcs = new DuoHashMap<String, String, AArc>();
+		for (AArc arc : arcs) {
+			if ((customer.get(arc.getFrom().getName()) == null && depots.get(arc.getFrom().getName()) == null)
+					|| (customer.get(arc.getTo().getName()) == null && depots.get(arc.getTo().getName()) == null)) {
+				System.out.println(customer.get(arc.getFrom().getName()));
+				System.out.println(depots.get(arc.getFrom().getName()));
+				System.out.println(customer.get(arc.getTo().getName()));
+				System.out.println(depots.get(arc.getTo().getName()));
+				this.arcs = new DuoHashMap<String, String, AArc>();
+				return false;
+			}
+			this.arcs.put(arc.getFrom().getName(), arc.getTo().getName(), arc);
 		}
-		return result;
-	}
-
-	@Override
-	public List<AVertice> getVertices() {
-		List<AVertice> allVertices = new ArrayList<AVertice>();
-		allVertices.addAll(consumer.values());
-		allVertices.addAll(depots.values());
-		return allVertices;
-	}
-
-	@Override
-	public void setConfig(Config config) {
-		if(config != null) {
-			this.config = config;
-		}
-	}
-
-	@Override
-	public Config getConfig() {
-		if(this.config == null) {
-			this.config = Config.createNewConfig();
-		}
-		return this.config;
+		return true;
 	}
 
 }
