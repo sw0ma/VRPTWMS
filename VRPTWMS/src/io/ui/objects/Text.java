286ad1b0e279ffdd34889fd92bd663ae9c6c23f4
Michael Walter <mwlter@aol.com>
2015-04-14 17:24:04 +0200

    instances have now a name

---
+++ VRPTWMS/src/io/ui/objects/Text.java
@@ -0,0 +1,29 @@
+package io.ui.objects;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import io.ui.IPaintable;
+
+public class Text implements IPaintable {
+	
+	private int x;
+	private int y;
+	private String text;
+	private Color color;
+	
+	public Text(int x, int y, String text, Color color) {
+		this.x = x;
+		this.y = y;
+		this.text = text;
+		this.color = color;
+	}
+
+	@Override
+	public void paintObject(Graphics2D g) {
+		g.setColor(color);
+		g.drawString(text, x, y);
+
+	}
+
+}
